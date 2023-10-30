ac34ef7c64e9b008ef2d35a7724addca95d9ca28
Atith794 <68183452+Atith794@users.noreply.github.com>
2023-10-30 22:40:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+
+
+// let isuzu = document.getELementById("sizeInfo");
+
+// isuzu.innerHTML = `<h1>Width: ${width.textContent} and Height: ${height.textContent}</h1>`;
+function updateSize() {
+	let width = window.innerWidth();
+	let height = window.innerHeight();
+	document.getElementById("width").textContent=width;
+	document.getElementById("height").textContent=height;
+}
+
+updateSize();
+
+window.addEventListener("resize",updateSize);
