1363b88defb4a5954bc0fbd06be23dc08acf33ec
Atith794 <68183452+Atith794@users.noreply.github.com>
2023-10-30 22:44:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,8 +5,8 @@
 
 // isuzu.innerHTML = `<h1>Width: ${width.textContent} and Height: ${height.textContent}</h1>`;
 function updateSize() {
-	let width = window.innerWidth();
-	let height = window.innerHeight();
+	let width = window.innerWidth;
+	let height = window.innerHeight;
 	document.getElementById("width").textContent=width;
 	document.getElementById("height").textContent=height;
 }
